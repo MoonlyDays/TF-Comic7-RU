198520eeef45d5af894788fe0e527a46d2c90f07
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2024-12-23 14:31:31 +0200

    tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			background: 'url(assets/main_bg.jpg)'
+		},
+	},
+	plugins: [],
+}
